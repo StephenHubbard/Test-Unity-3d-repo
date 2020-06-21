6f29fac77ada366f4594eabee2eef454d2fe7757
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-06-21 15:33:46 -0600

    adding level progress

--- Block.cs
+++ Block.cs
@@ -7,14 +7,29 @@ public class Block : MonoBehaviour
 
     [SerializeField] AudioClip breakSound;
 
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+        // Debug.Log(collision.gameObject.name);
         if (collision.gameObject.CompareTag("Ball"))
         {
-            AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-            Destroy(gameObject);
+            DestroyBlock();
         }
 
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, .3f);
+        Destroy(gameObject);
+        level.BlockDestroy();
     }
 }
---
+++ Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //parameters
+    [SerializeField] int breakableBlocks;
+
+    // cached references
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroy()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+}
